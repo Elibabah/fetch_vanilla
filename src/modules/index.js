4faffa8f8892cf2d7c1a9e084390696adf3a4e66
elibabah <delias16.hernandez@gmail.com>
2022-08-24 22:59:06 -0500

    all app

---
+++ src/modules/index.js
@@ -0,0 +1,28 @@
+const getPersonajes = (callback) => {
+    const dataPersonajes = fetch("https://rickandmortyapi.com/api/character")
+    dataPersonajes
+        .then((resp) => resp.json())
+        .then((data) => {
+            callback(data)
+        })
+}
+
+getPersonajes(data => {
+    console.log(data.results)
+    data.results.map((personaje) => {
+        const pintar = document.createRange().createContextualFragment(
+            `<article>
+                <div class="image-container">
+                    <img src="${personaje.image}" alt="${personaje.name}">
+                </div>
+
+                <h2>${personaje.name}</h2>
+                <span>${personaje.status}</span>
+                <span>${personaje.species}</span>
+            </article>`
+        )
+        const main = document.querySelector("main");
+        main.append(pintar)
+    })
+
+})
